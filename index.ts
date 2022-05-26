98d41c2bcc48919332632886652329f2d88660cc
SamerNoufal <samer.noufal13@gmail.com>
2022-05-26 09:00:59 +0300

    create projectassigment

---
+++ index.ts
@@ -0,0 +1,44 @@
+import express from'express';
+const app=express();
+const port=process.env.PORT || 3000;
+import db from './models';
+import {users} from './seeders/users';
+import {categories} from './seeders/categories';
+import {projectassignments} from './seeders/projectassigments';
+
+
+const createUsers=()=>{
+    users.map(user=>{
+        db.User.create(user)
+      
+    })  
+   
+}
+
+const createCategories=()=>{
+    categories.map(category=>{
+        db.Category.create(category)
+      
+    })  
+   
+}
+
+const createProjectAssignments=()=>{
+    projectassignments.map(projectassignment=>{
+        db.ProjectAssignment.create(projectassignment)
+      
+    })  
+   
+}
+
+
+// createUsers();
+// createCategories();
+// createProjectAssignments();
+
+
+db.sequelize.sync().then(() => {
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}`)
+    })
+})

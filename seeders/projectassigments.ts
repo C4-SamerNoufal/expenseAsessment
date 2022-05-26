8032e0d13732165ce46292a7dd889fac8d797bd1
SamerNoufal <samer.noufal13@gmail.com>
2022-05-26 08:56:21 +0300

    project assignment seeder

---
+++ seeders/projectassigments.ts
@@ -0,0 +1,20 @@
+export const projectassignments = [
+
+
+{
+    CategoryId:1,
+    UserId: 'b73d78b4-30f0-44cf-bfc3-8fdab011f4a6'
+},
+
+{
+    CategoryId:3,
+    UserId: 'b73d78b4-30f0-44cf-bfc3-8fdab011f4a6'
+},
+
+{
+    CategoryId:1,
+    UserId: 'ca9c2956-f96e-4e75-81a2-3e36593f09e7'
+},
+
+
+]

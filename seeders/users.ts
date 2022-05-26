e4f5eb3140205537fcd40cfaf7e2b2d6f04a15f5
SamerNoufal <samer.noufal13@gmail.com>
2022-05-26 06:41:27 +0300

    add users seeder

--- seeders/users.ts
+++ seeders/users.ts
@@ -4,12 +4,17 @@ import{v4 as uuidv4} from 'uuid';
 export const users = [
 
 {
+id: uuidv4(),
+name: 'Samer',
+email: 'samer.noufal13@gmail.com',
+password: '555'
+},
 
-}
-
-
-
-
-
-     
+{
+    id: uuidv4(),
+    name: 'Saleh',
+    email: 'saleh@gmail.com',
+    password: '555'
+    },
+ 
 ]

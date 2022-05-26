cbd9ee42f53beadaf1a8e43909d360f5760e5114
SamerNoufal <samer.noufal13@gmail.com>
2022-05-26 20:26:40 +0300

    add category controller

---
+++ controllers/categoriesControllers.ts
@@ -0,0 +1,19 @@
+const { db } = require("../models/");
+const { Category } = require("../models/category.ts");
+const { User } = require("../models/user.ts");
+
+const addCategory = async (req:any,res:any)=>{
+
+    const {userUuid,body} = req.body
+try{
+    const user = await User.findOne({where:{uuid:userUuid}})
+
+    const category = await Category.create({body,userId:userUuid.id})
+
+    return res.json(category)
+
+}catch(err){
+    console.log(err)
+    return res.status(500).json(err)
+}
+}

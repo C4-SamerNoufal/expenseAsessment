6943d061077e9ca3e9bbdc1b90e4e34cd7c6f855
SamerNoufal <samer.noufal13@gmail.com>
2022-05-27 07:40:43 +0300

    authentication file

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,6 +5,7 @@ const userRouter = express.Router();
 const {
     register,
     getAllUsers,
+    login,
   
   } = require("../controllers/UsersControllers");
 
@@ -12,5 +13,6 @@ const {
   //1- create end points for register
   userRouter.post("/register", register);
   userRouter.get("/", getAllUsers);
+  userRouter.post("/login", login);
 
   module.exports = { userRouter };
